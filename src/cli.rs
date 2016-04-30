cf973e4efc280983e88b4f84d23619acd3aa5180
Jason Campbell <jasoncampbell@google.com>
2016-04-30 11:21:23 -0700

    Refactor for cli tests.

--- src/cli.rs
+++ src/cli.rs
@@ -1,137 +1,58 @@
-extern crate clap;
+// use std::io::Error;
 
-use types::{ToResult, ToError};
-use std::path::PathBuf;
 use std::env;
-use std::fs;
-use std::option::Option;
+use std::env::ArgsOs;
+use clap;
+use clap::{App, Arg};
+
+
+pub type ToResult<T> = Result<T, ToError>;
 
 #[derive(Debug)]
-pub enum Action {
-    Get,
-    Put,
-    Delete,
-    List,
-    Last,
-    ChangeDirectory,
-}
-
-pub fn parse_matches(matches: clap::ArgMatches) -> ToResult<Request> {
-    let (get, put, delete, list, last) = (matches.is_present("get"),
-                                          matches.is_present("put"),
-                                          matches.is_present("delete"),
-                                          matches.is_present("list"),
-                                          matches.is_present("last"));
-
-    let action = match (get, put, delete, list, last) {
-        (true, _, _, _, _) => Action::Get,
-        (_, true, _, _, _) => Action::Put,
-        (_, _, true, _, _) => Action::Delete,
-        (_, _, _, true, _) => Action::List,
-        (_, _, _, _, true) => Action::Last,
-        _ => Action::ChangeDirectory,
-    };
-
-    // to --save,--put # Save current dir as a bookmark
-    // to --save,--put foo # Save current directory as bookmark foo
-    // to --delete foo # Delete bookmark foo
-    // to --info foo # Show details of the bookmark foo
-    // to --list # List all the bookmarks
-    // to foo # Go to the foo bookmark
-    // reserve "-" so it can be used later.
-    // to - # go to the last bookmark you visited
-
-    println!("action: {:?}", action);
-
-    let directory = match resolve(matches.value_of("directory")) {
-        Ok(value) => value,
-        Err(err) => return Err(err),
-    };
-
-    println!("directory: {:?}", directory);
-
-    let dir = directory.clone();
-    let basedir = match dir.file_stem() {
-        Some(value) => value.to_str(),
-        None => panic!("TODO: I dont even"),
-    };
-
-    let name = matches.value_of("name")
-                      .unwrap_or(basedir.unwrap())
-                      .to_string();
-
-    println!("basedir {:?}", basedir);
-    println!("name {:?}", name);
-
-    let req = Request::new(name, directory, action);
-    return Ok(req);
-}
-
-#[cfg(test)]
-mod test {
-    #[test]
-    fn test_example() {
-        assert!(true, true);
-    }
-}
-
-fn resolve(pathname: Option<&str>) -> ToResult<PathBuf> {
-    let pathname = match pathname {
-        Some(value) => value,
-        None => "",
-    };
-
-    let mut absolute = match env::current_dir() {
-        Ok(value) => value,
-        Err(err) => return Err(ToError::Io(err)),
-    };
-
-    // Don't default to "." since it will be a literal translation creating
-    // dumb directories like "/foo/bar/."
-    if pathname != "." {
-        absolute.push(pathname);
-    }
-
-    // TODO: figure out a better way to deal with this moved value.
-    let resolved = absolute.clone();
-
-    match exists(absolute) {
-        Ok(value) => {
-            match value {
-                true => return Ok(resolved),
-                false => panic!("TODO: Does not exist error!"),
-            }
-        }
-        Err(err) => Err(err),
-    }
-}
-
-fn exists(directory: PathBuf) -> ToResult<bool> {
-    let metadata = match fs::metadata(directory) {
-        Ok(value) => value,
-        Err(err) => return Err(ToError::Io(err)),
-    };
-
-    if metadata.is_dir() {
-        return Ok(true);
-    }
-
-    return Ok(false);
+pub enum ToError {
+    // Io(Error)
 }
 
 #[derive(Debug)]
 pub struct Request {
-    pub name: String,
-    pub directory: PathBuf,
-    pub action: Action,
+
 }
 
 impl Request {
-    fn new(name: String, directory: PathBuf, action: Action) -> Request {
-        Request {
-            name: name,
-            directory: directory,
-            action: action,
-        }
+    fn new() -> Request {
+        Request {}
+    }
+}
+
+pub fn get_request() -> ToResult<Request> {
+    let args = env::args_os();
+    return get_request_from(args);
+
+}
+
+pub fn get_request_from(args: ArgsOs) -> ToResult<Request> {
+    let yaml = load_yaml!("cli.yml");
+    let app = App::from_yaml(yaml);
+    let matches = app.get_matches_from(args);
+
+    let request = Request::new();
+    return Ok(request);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn basic() {
+        let request = get_request();
+        assert!(request.is_ok());
+    }
+
+    #[test]
+    fn basic_from_vec() -> ToResult<Request> {
+        let args = vec!["foo", "bar"];
+        let request = get_request_from(args);
+        assert!(request.is_ok());
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,93 +1,13 @@
-mod types;
-mod dir;
-mod database;
-mod cli;
-
 #[macro_use]
 extern crate clap;
-extern crate bincode;
-extern crate rustc_serialize;
 
-use database::Database;
-use cli::Action;
-use clap::{App, Arg};
+mod cli;
 
 fn main() {
-    let version = crate_version!();
-    let actions = [Arg::with_name("get")
-                       .help("Show bookmark information")
-                       .long("info")
-                       .short("i"),
-
-                   // to --save,--put # Save current dir as a bookmark
-                   // to --save,--put foo # Save current directory as bookmark foo
-                   Arg::with_name("put")
-                       .help("Save bookmark")
-                       .long("save")
-                       .short("s"),
-
-                   // to --delete foo # Delete bookmark foo
-                   Arg::with_name("delete")
-                       .help("Delete bookmark")
-                       .long("delete")
-                       .short("d"),
-
-                   Arg::with_name("list")
-                       .help("List all bookmarks")
-                       .long("list")
-                       .short("l"),
-
-                   Arg::with_name("last")
-                       .help("Change current working directory to last")
-                       .long("last")];
-    let matches = App::new("to")
-                      .about("Bookmark directories")
-                      .version(version)
-                      .author("Jason Campbell <jason@artifact.sh>")
-                      .arg(Arg::with_name("name")
-                               .help("Name of the bookamrk")
-                               .index(1))
-                      .arg(Arg::with_name("directory")
-                               .help("Path of the bookamrk")
-                               .index(2))
-                      .args(&actions)
-                      .get_matches();
-
-    let request = match cli::parse_matches(matches) {
+    let request = match cli::get_request() {
         Ok(value) => value,
         Err(err) => panic!(err),
     };
 
     println!("CLI Request: {:?}", request);
-
-    // NEXT: Store get, put, delete, list
-    let config_dir = match dir::config() {
-        Ok(value) => value,
-        Err(err) => panic!(err),
-    };
-
-    let mut store = match Database::open(config_dir) {
-        Ok(db) => db,
-        Err(e) => panic!("failed to open database: {:?}", e),
-    };
-
-    println!("store {:?}", store);
-    let result = match request.action {
-        Action::Put => store.put(request.name, request.directory),
-        Action::Get => {
-            match store.get(request.name) {
-                Ok(bookmark) => {
-                    println!("bookmark {:?}", bookmark);
-                    Ok(())
-                }
-                Err(err) => Err(err),
-            }
-        }
-        _ => panic!("NOT IMPLEMENTED!"),
-    };
-
-    match result {
-        Ok(value) => println!("success! {:?}", value),
-        Err(err) => panic!(err),
-    }
 }
