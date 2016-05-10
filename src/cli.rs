124fa81f6c29b45d745a98aa2e26649fd1793c49
Jason Campbell <jasoncampbell@google.com>
2016-05-09 17:27:20 -0700

    Adds assertion for Action.

--- src/cli.rs
+++ src/cli.rs
@@ -1,5 +1,6 @@
-// use std::io::Error;
 use clap::{App, ArgMatches};
+use log;
+
 extern crate env_logger;
 
 
@@ -7,18 +8,39 @@ pub type ToResult<T> = Result<T, ToError>;
 
 #[derive(Debug)]
 pub enum ToError {
-    // Io(Error)
+    Log(log::SetLoggerError),
+}
+
+// TODO: add custom displays for these errors.
+// * SEE: https://jadpole.github.io/rust/many-error-types
+// * SEE: http://lucumr.pocoo.org/2014/11/6/error-handling-in-rust/
+impl From<log::SetLoggerError> for ToError {
+    fn from(err: log::SetLoggerError) -> ToError {
+        ToError::Log(err)
+    }
 }
 
 #[derive(Debug)]
 pub struct Request {
-
+    // pub name: String,
+    // pub directory: PathBuf,
+    pub action: Action,
 }
 
 impl Request {
     fn new() -> Request {
-        Request {}
+        Request { action: Action::Put }
     }
+}
+
+#[derive(Debug, PartialEq)]
+pub enum Action {
+    Get,
+    Put,
+    Delete,
+    List,
+    Last,
+    ChangeDirectory,
 }
 
 pub fn get_request() -> ToResult<Request> {
@@ -38,7 +60,7 @@ pub fn get_request_from(args: Vec<&str>) -> ToResult<Request> {
 }
 
 fn get(matches: ArgMatches) -> ToResult<Request> {
-    info!("cli parsing matches: {:?}", matches);
+    info!("building request from clap::{:?}", matches);
 
     let request = Request::new();
     return Ok(request);
@@ -49,18 +71,31 @@ mod tests {
     use super::*;
     extern crate env_logger;
 
+    fn run(mut args: Vec<&str>) -> Request {
+        let logger_result = env_logger::init();
+        assert!(logger_result.is_ok());
+
+        args.insert(0, "to");
+
+        let result = get_request_from(args);
+        assert!(result.is_ok());
+
+        return result.unwrap();
+    }
+
     #[test]
     fn basic() {
-        let _ = env_logger::init();
         let request = get_request();
         assert!(request.is_ok());
     }
 
     #[test]
     fn basic_from_vec() -> () {
-        let _ = env_logger::init();
         let args = vec!["foo", "bar"];
-        let request = get_request_from(args);
-        assert!(request.is_ok());
+        let request = run(args);
+
+        // assert_eq!(request.name, "foo");
+        // assert_eq!(request.directory, "bar");
+        assert_eq!(request.action, Action::Put);
     }
 }
--- src/types.rs
+++ src/types.rs
@@ -4,7 +4,6 @@ use std::path::PathBuf;
 
 pub type ToResult<T> = Result<T, ToError>;
 
-// SEE: http://lucumr.pocoo.org/2014/11/6/error-handling-in-rust/
 #[derive(Debug)]
 pub enum ToError {
     Io(io::Error),
