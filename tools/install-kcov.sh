1bdcb179e376450b33754c9139513d05a4d491a6
Jason Campbell <jasoncampbell@google.com>
2017-04-19 23:20:40 -0700

    Add install kcov script

---
+++ tools/install-kcov.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -eu
+
+KCOV_VERSION=33
+
+rm -rf v${KCOV_VERSION}.tar.gz kcov-${KCOV_VERSION}/
+
+wget https://github.com/SimonKagstrom/kcov/archive/v${KCOV_VERSION}.tar.gz
+tar xzf v${KCOV_VERSION}.tar.gz
+cd kcov-${KCOV_VERSION}
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo ..
+make
+mkdir -p ~/.cargo/bin
+cp src/kcov src/libkcov_sowrapper.so ~/.cargo/bin
