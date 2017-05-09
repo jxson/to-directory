006a8b62f8a37c9a2ba7ed92e5db88b23c658da8
Jason Campbell <jasoncampbell@google.com>
2017-05-08 23:50:33 -0700

    More travis/coveralls debug info

--- tools/coveralls.sh
+++ tools/coveralls.sh
@@ -1,4 +1,5 @@
-if [[ $TRAVIS_RUST_VERSION = "stable" ]]; then
+echo $TRAVIS_RUST_VERSION
+if [[ $TRAVIS_RUST_VERSION -eq "stable" ]]; then
   echo "Sending info to coveralls"
   cargo kcov --verbose --coveralls
 fi
