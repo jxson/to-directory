8a4327573490eb9ff3990f017bf0de16a31a2776
Jason Campbell <jasoncampbell@google.com>
2017-09-24 18:21:21 -0700

    Debug flakey test

--- tests/to_test.rs
+++ tests/to_test.rs
@@ -5,11 +5,10 @@ use assert_cli::Assert;
 
 #[test]
 fn to_init_flag() {
-    let test = Assert::main_binary()
+    Assert::main_binary()
         .with_args(&["--init"])
         .succeeds()
         .and()
         .prints(include_str!("../src/to.sh"))
-        .execute();
-    assert!(test.is_ok());
+        .unwrap();
 }
