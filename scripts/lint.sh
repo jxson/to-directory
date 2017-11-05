9c37e6623801b61562051aaa22678175712b7f24
Jason Campbell <jasoncampbell@google.com>
2017-11-05 12:06:42 -0800

    Force install rustfmt on Travis

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -3,9 +3,9 @@ set -e
 source "$(git rev-parse --show-toplevel)/scripts/common.sh"
 
 function main() {
-  if [[ $TRAVIS_RUST_VERSION -eq "nightly" && -z $(which rustfmt) ]]; then
+  if [[ $TRAVIS_RUST_VERSION -eq "nightly" ]]; then
     echo "==> installing rustfmt-nightly"
-    cargo install rustfmt-nightly
+    cargo install rustfmt-nightly --force
   fi
 
   echo "==> checking formatting"
