066bac92632e57b3fdbdbc00c96ce10c299d61b2
Jason Campbell <jasoncampbell@google.com>
2018-02-06 22:43:17 -0800

    Adds integration test scaffolding

--- examples/integration-tests.rs
+++ examples/integration-tests.rs
@@ -1,47 +1,79 @@
 extern crate assert_cli;
 extern crate tap_rust;
+extern crate glob;
+extern crate bincode;
+#[macro_use] extern crate error_chain;
+#[macro_use] extern crate serde_derive;
+extern crate serde;
+extern crate serde_json;
 
-use assert_cli::Assert;
-use tap_rust::tap_writer::TapWriter;
+// use bincode;
+use glob::glob;
+// use assert_cli::Assert;
+// use tap_rust::tap_writer::TapWriter;
+use std::env;
+use std::path::PathBuf;
+use std::fs::File;
 
 // https://stackoverflow.com/questions/35711044/specify-binary-only-dependencies
 // https://github.com/Cigna/TAP-Rust
 // https://github.com/killercup/assert_cli/
 
 fn main() {
-    let test = Assert::main_binary()
-        .with_args(&["--init"])
-        .succeeds()
-        .and()
-        .stdout()
-        .contains(include_str!("../src/to.sh"))
-        .execute();
+    let dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+    let path = format!("{}/tests/integration/*.json", dir);
 
-    let writer = TapWriter::new("Example TAP stream");
-
-    writer.plan(1, 1);
-
-    writer.name();
-
-    match test {
-        Ok(_) => writer.ok(1, "--init"),
-        Err(err) => {
-            writer.not_ok(1, "--init");
-            let message = format!("{}", err);
-            writer.diagnostic(message.as_str());
-        },
+    for entry in glob(&path).expect("Failed to read glob pattern") {
+        match entry {
+            Ok(path) => test(&path),
+            Err(e) => println!("{:?}", e),
+        }
     }
 
-    // // Print out some test results
-    // ;
-    // writer.ok(2, "Bamboo");
-    // writer.ok(3, "Curry");
-    // // This one failed, so explain why with a diagnostic line
-    // writer.not_ok(4, "Noodle");
-    // writer.diagnostic("The above test failed because of XYZ reason");
-    // writer.ok(5, "Tree");
+    // let test = Assert::main_binary()
+    //     .with_args(&["--init"])
+    //     .succeeds()
+    //     .and()
+    //     .stdout()
+    //     .contains(include_str!("../src/to.sh"))
+    //     .execute();
     //
-    // // uh oh! something went horribly wrong and we need to stop before
-    // // we print out the results from test 6!
-    // writer.bail_out_with_message("Destabilized warp core! Can't continue!");
+    // let writer = TapWriter::new("Example TAP stream");
+    //
+    // writer.plan(1, 1);
+    //
+    // writer.name();
+    //
+    // match test {
+    //     Ok(_) => writer.ok(1, "--init"),
+    //     Err(err) => {
+    //         writer.not_ok(1, "--init");
+    //         let message = format!("{}", err);
+    //         writer.diagnostic(message.as_str());
+    //     },
+    // }
+}
+
+error_chain!{
+    foreign_links {
+        IOError(std::io::Error) #[doc = "Error during IO"];
+        SerdeError(serde_json::Error);
+    }
+}
+
+#[derive(Serialize, Deserialize, PartialEq, Debug)]
+struct Spec {
+    pub name: String,
+    pub status: u32,
+}
+
+fn test(path: &PathBuf) {
+    let spec = hydrate(path);
+    println!("{:?}", spec);
+}
+
+fn hydrate(path: &PathBuf) -> Result<Spec> {
+    let file = try!(File::open(path));
+    let spec = try!(serde_json::from_reader(file));
+    Ok(spec)
 }
