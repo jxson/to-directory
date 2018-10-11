02549fdfc6440f54b33e3e3676de300a875c6fc4
Jason Campbell <jasoncampbell@google.com>
2018-10-11 09:51:59 -0700

    WIP example is working

--- examples/failure.rs
+++ examples/failure.rs
@@ -1,23 +1,26 @@
 #[macro_use]
 extern crate failure;
-extern crate dirs;
+extern crate to;
 
-use failure::{Context, Fail, ResultExt};
+use failure::{Backtrace, Context, Fail, ResultExt};
 use std::env;
 use std::fmt;
 use std::fmt::Display;
 use std::io;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
 fn main() {
-    let does_not_exist = PathBuf::from("does-not-exist");
+    let path = PathBuf::from("does-not-exist");
 
     // for cause in Fail::iter_causes(&resolve(does_not_exist).unwrap_err()) {
     //     println!("{}", cause);
     // }
 
-    let err = resolve(does_not_exist).unwrap_err();
-    println!("{:?}, {:?}", err.cause(), err.backtrace())
+    match resolve(path) {
+        Ok(path) => println!("success: {:?}", path),
+        Err(err) => println!("failure: {}", pretty_error(&err)),
+    }
+    // println!("{:?}, {:?}", err.cause(), err.backtrace())
 }
 
 #[derive(Debug, Fail)]
@@ -25,66 +28,77 @@ pub struct Error {
     ctx: Context<ErrorKind>,
 }
 
-#[derive(Debug, Fail)]
-pub enum ErrorKind {
-    #[fail(display = "{:?}", _0)]
-    Path(PathBuf),
-    #[fail(display = "{}", _0)]
-    IO(#[fail(cause)] io::Error),
-}
+impl Error {
+    pub fn io(err: io::Error) -> Error {
+        Error::from(ErrorKind::IO(err.to_string()))
+    }
 
-impl Display for Error {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        Display::fmt(&self.ctx, f)
+    pub fn path<P: AsRef<Path>>(path: P) -> Error {
+        let kind = ErrorKind::Path(path.as_ref().to_path_buf());
+        Error::from(kind)
     }
 }
 
-impl From<io::Error> for Error {
-    fn from(err: io::Error) -> Error {
-        Error {
-            ctx: Context::new(ErrorKind::IO(err)),
-        }
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        self.ctx.fmt(f)
     }
 }
 
 impl From<ErrorKind> for Error {
     fn from(kind: ErrorKind) -> Error {
-        Error {
-            ctx: Context::new(kind),
-        }
+        Error::from(Context::new(kind))
     }
 }
 
 impl From<Context<ErrorKind>> for Error {
     fn from(ctx: Context<ErrorKind>) -> Error {
-        Error { ctx: ctx }
+        Error { ctx }
     }
 }
 
+#[derive(Debug, Fail)]
+pub enum ErrorKind {
+    Path(PathBuf),
+    IO(String),
+}
+
+impl fmt::Display for ErrorKind {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            ErrorKind::Path(ref path) => write!(f, "path: {}", path.display()),
+            ErrorKind::IO(ref msg) => write!(f, "IO error: {}", msg),
+        }
+    }
+}
+
+/// Return a prettily formatted error, including its entire causal chain.
+fn pretty_error(err: &failure::Error) -> String {
+    let mut pretty = err.to_string();
+    let mut prev = err.as_fail();
+    while let Some(next) = prev.cause() {
+        pretty.push_str(": ");
+        pretty.push_str(&next.to_string());
+        prev = next;
+    }
+
+    pretty.push_str(&format!("{}", err.backtrace()));
+    pretty
+}
+
 pub fn resolve(path: PathBuf) -> Result<PathBuf, failure::Error> {
-    env::current_dir()
-        .map(|mut full_path| {
-            full_path.push(path.to_path_buf());
-            full_path
-        }).and_then(|p| {
-            if p.exists() {
-                Err(io::Error::from(io::ErrorKind::NotFound))
-            } else {
-                Ok(p)
-            }
-        }).and_then(|p| p.canonicalize())
-    // .map_err(Error::from)
-    // .context(ErrorKind::Path(path))
+    let path = env::current_dir()
+        .map(|mut p| {
+            p.push(path.to_path_buf());
+            p
+        })
+        .map_err(Error::io)
+        .context("failed to get current dir")?;
 
-    // if !absolute.exists() {
-    //     let err = Error::from(ErrorKind::Path(path));
-    //     return Err(err);
-    // }
+    let path = path
+        .canonicalize()
+        .map_err(Error::io)
+        .with_context(|_| Error::path(path))?;
 
-    // Ok(absolute)
-
-    // match absolute.canonicalize() {
-    //     Ok(value) => Ok(value),
-    //     Err(err) => bail!(Error::from(err)),
-    // }
+    Ok(path)
 }
